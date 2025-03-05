1b7873d27c024e526cb244a93f779eb3bb847562
PZPlayer <141235888+PZPlayer@users.noreply.github.com>
2025-03-06 00:49:17 +0300

    AddedAssetMovement

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -5,38 +5,39 @@ namespace Hydra.Player
 {
     public class Movement : MonoBehaviour
     {
-        [SerializeField] private float _speed;
-        [SerializeField] private float _jumpPower;
+        //    [SerializeField] private float _speed;
+        ////    [SerializeField] private float _jumpPower;
 
-        private Rigidbody rb;
+        //    private Rigidbody rb;
 
         void Start ()
         {
-            rb = GetComponent<Rigidbody>();
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
         }
 
-        void FixedUpdate ()
-        {
-            float axisX = Input.GetAxisRaw("Horizontal");
-            float axisY = Input.GetAxisRaw("Vertical");
+        //void FixedUpdate ()
+        //{
+        //    float axisX = Input.GetAxisRaw("Horizontal");
+        //    float axisY = Input.GetAxisRaw("Vertical");
 
-            Vector2 direction = new Vector2(axisX, axisY).normalized * Time.deltaTime;
-            Quaternion targetRotation = Quaternion.LookRotation(new Vector3(direction.x, transform.position.y, direction.y));
-            print(" " +  targetRotation + " " + direction);
-            rb.MovePosition(transform.position + new Vector3(direction.x * targetRotation.x, 0, direction.y * targetRotation.z) * _speed);
-            RotatePlayer(direction);
-        }
+        //    Vector2 direction = new Vector2(axisX, axisY).normalized * Time.deltaTime;
+        //    Quaternion targetRotation = Quaternion.LookRotation(new Vector3(direction.x, transform.position.y, direction.y));
+        //    print(" " +  targetRotation + " " + direction);
+        //    rb.velocity = new Vector3(direction.x * _speed, rb.velocity.y, direction.y * _speed);
+        //    if(axisX != 0 || axisY != 0) RotatePlayer(direction);
+        //}
 
-        void RotatePlayer(Vector2 dir)
-        {
-            Quaternion targetRotation = Quaternion.LookRotation(new Vector3(dir.x, transform.position.y, dir.y));
+        //void RotatePlayer(Vector2 dir)
+        //{
+        //    Quaternion targetRotation = Quaternion.LookRotation(new Vector3(dir.x, transform.position.y, dir.y));
 
-            float targetYRotation = targetRotation.eulerAngles.y;
+        //    float targetYRotation = targetRotation.eulerAngles.y;
 
-            Quaternion finalRotation = Quaternion.Euler(0, targetYRotation, 0);
+        //    Quaternion finalRotation = Quaternion.Euler(0, targetYRotation, 0);
 
-            transform.rotation = Quaternion.Slerp(transform.rotation, finalRotation, 5 * Time.deltaTime);
-        }
+        //    transform.rotation = Quaternion.Slerp(transform.rotation, finalRotation, 5 * Time.deltaTime);
+        //}
 
 
 
