0071492dc67325f8689d0e02f0cd1d9842c00f7f
PZPlayer <141235888+PZPlayer@users.noreply.github.com>
2025-04-01 19:10:53 +0300

    InvComplete

--- Assets/Scripts/Player/Inventory.cs
+++ Assets/Scripts/Player/Inventory.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Gamecraft.Player
 {
@@ -11,11 +12,21 @@ namespace Gamecraft.Player
     {
         public GameObject ItemGameObject;
         public Item ItemInfo;
+        public int SlotIndex;
     }
     public class Inventory : MonoBehaviour
     {
         [SerializeField] private ItemOnScene[] _item;
+        [SerializeField] private Image _firstInvImage;
+        [SerializeField] private Image _secondInvImage;
+        [SerializeField] private Image _thirdInvImage;
         private int selected = 0;
+
+
+        void Start ()
+        {
+            UpdateQuickInv();
+        }
 
         public void ChangeItem(int index)
         {
@@ -28,6 +39,7 @@ namespace Gamecraft.Player
             selected = index;
             print(selected);
             UpdateItem();
+            UpdateQuickInv();
         }
 
         public void UseItem()
@@ -49,6 +61,28 @@ namespace Gamecraft.Player
                 return;
             }
             _item[selected].ItemGameObject.SetActive(true);
+        }
+
+        private void UpdateQuickInv()
+        {
+            foreach (ItemOnScene itm in _item)
+            {
+                if (itm.ItemInfo)
+                {
+                    switch (itm.SlotIndex)
+                    {
+                        case 0:
+                            _firstInvImage.sprite = itm.ItemInfo.Image;
+                            break;
+                        case 1:
+                            _secondInvImage.sprite = itm.ItemInfo.Image;
+                            break;
+                        case 2:
+                            _thirdInvImage.sprite = itm.ItemInfo.Image;
+                            break;
+                    } 
+                }
+            }
         }
 
         private void HideAllItems()
